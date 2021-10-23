6959575e8035672bcf711e81ceeefa9c063eeacd
Emerson MX <emersonmx@gmail.com>
2021-10-23 18:22:43 -0300

    Add multiply operator

--- tests/operators/test_add.py
+++ tests/operators/test_add.py
@@ -1,6 +1,10 @@
+import itertools
+
 import pytest
 
 from calculator import DefaultStack, Number, OperatorError, add
+
+DEFAULT_NUMBERS = itertools.product([-5, -1, 0, -1, 5], repeat=2)
 
 
 def test_should_requires_two_numbers() -> None:
@@ -20,16 +24,22 @@ def test_should_requires_two_numbers() -> None:
     assert stack.top() == Number(1)
 
 
-def test_should_add_two_numbers() -> None:
+@pytest.mark.parametrize(
+    "a, b, result",
+    [
+        *[(a, b, a + b) for a, b in DEFAULT_NUMBERS],
+    ],
+)
+def test_should_add_two_numbers(a: int, b: int, result: int) -> None:
     stack = DefaultStack()
 
-    stack.push(Number(1))
-    stack.push(Number(1))
+    stack.push(Number(a))
+    stack.push(Number(b))
 
     add(stack)
 
     assert stack.size() == 1
-    assert stack.top() == Number(2)
+    assert stack.top() == Number(result)
 
 
 def test_should_pop_two_numbers_and_push_the_result() -> None:
--- tests/operators/test_multiply.py
+++ tests/operators/test_multiply.py
@@ -2,7 +2,7 @@ import itertools
 
 import pytest
 
-from calculator import DefaultStack, Number, OperatorError, subtract
+from calculator import DefaultStack, Number, OperatorError, multiply
 
 DEFAULT_NUMBERS = itertools.product([-5, -1, 0, -1, 5], repeat=2)
 
@@ -11,14 +11,14 @@ def test_should_requires_two_numbers() -> None:
     stack = DefaultStack()
 
     with pytest.raises(OperatorError):
-        subtract(stack)
+        multiply(stack)
 
     assert stack.size() == 0
 
     stack.push(Number(1))
 
     with pytest.raises(OperatorError):
-        subtract(stack)
+        multiply(stack)
 
     assert stack.size() == 1
     assert stack.top() == Number(1)
@@ -27,16 +27,16 @@ def test_should_requires_two_numbers() -> None:
 @pytest.mark.parametrize(
     "a, b, result",
     [
-        *[(a, b, a - b) for a, b in DEFAULT_NUMBERS],
+        *[(a, b, a * b) for a, b in DEFAULT_NUMBERS],
     ],
 )
-def test_should_subtract_two_numbers(a: int, b: int, result: int) -> None:
+def test_should_multiply_two_numbers(a: int, b: int, result: int) -> None:
     stack = DefaultStack()
 
     stack.push(Number(a))
     stack.push(Number(b))
 
-    subtract(stack)
+    multiply(stack)
 
     assert stack.size() == 1
     assert stack.top() == Number(result)
@@ -49,7 +49,7 @@ def test_should_pop_two_numbers_and_push_the_result() -> None:
     stack.push(Number(2))
     stack.push(Number(3))
 
-    subtract(stack)
+    multiply(stack)
 
     assert stack.size() == 2
-    assert list(stack) == list(map(Number, [1, -1]))
+    assert list(stack) == list(map(Number, [1, 6]))
